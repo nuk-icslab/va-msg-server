30f085faef27f98ff9f68a6233c82316908ecd07
ifTNT <iftnt1999@gmail.com>
2022-01-15 01:04:48 +0800

    Mock code of new design

--- src/obj_server.js
+++ src/obj_server.js
@@ -36,55 +36,84 @@ class ObjServer {
     });
 
     socket.on("message", async (data) => {
-      let { user_id, group_id, time, message } = data;
-      message = message.content;
-      time;
-      // Store the message into database
-      let new_msg = new Message({
+      let { user_id, group_id, payload } = data;
+      // [TODO] Check whether user_id is in the VAL group
+      let user_id_in_val_group = true;
+      if (!user_id_in_val_group) {
+        socket.emit("rejeted", { reason: "Not in the VAL group" });
+        return;
+      }
+
+      // [TODO] Ask VA server whether the message should be released
+      let release_msg = true;
+      if (!release_msg) {
+        socket.emit("rejeted", { reason: "Reject by VA server" });
+      }
+
+      this.io.to(group_id).emit("message", {
         user_id,
         group_id,
-        message,
-        time,
+        payload,
       });
-      await new_msg.save();
 
-      // Forward to subscribers
-      if (group_id in this.subscrib_list) {
-        let subscribe_list = this.subscrib_list[group_id];
+      // message = message.content;
+      // time;
+      // // Store the message into database
+      // let new_msg = new Message({
+      //   user_id,
+      //   group_id,
+      //   message,
+      //   time,
+      // });
+      // await new_msg.save();
 
-        subscribe_list.forEach((socket_id) => {
-          try {
-            console.log(`Forward to ${socket_id}`);
-            this.sockets[socket_id].emit("message", {
-              user_id,
-              group_id,
-              time,
-              message,
-            });
-          } catch (e) {}
-        });
-      }
+      // // Forward to subscribers
+      // if (group_id in this.subscrib_list) {
+      //   let subscribe_list = this.subscrib_list[group_id];
+
+      //   subscribe_list.forEach((socket_id) => {
+      //     try {
+      //       console.log(`Forward to ${socket_id}`);
+      //       this.sockets[socket_id].emit("message", {
+      //         user_id,
+      //         group_id,
+      //         time,
+      //         message,
+      //       });
+      //     } catch (e) {}
+      //   });
+      // }
     });
     socket.on("subscribe", async (data) => {
       let { group_id } = data;
-      if (!(group_id in this.subscrib_list)) {
-        this.subscrib_list[group_id] = [];
+
+      // [TODO] Check whether the user is in the VAL group
+      let user_in_val_group = true;
+      if (user_in_val_group) {
+        socket.join(group_id);
+      } else {
+        socket.emit("rejeted", { reason: "Not in the VAL group" });
       }
-      this.subscrib_list[group_id].push(socket.id);
-      // Look for all of the messages for the group
-      let msgs = await Message.find({ group_id });
-      // Send to the incoming client
-      msgs.forEach((msg) => {
-        socket.emit("message", msg);
-      });
+
+      // if (!(group_id in this.subscrib_list)) {
+      //   this.subscrib_list[group_id] = [];
+      // }
+      // this.subscrib_list[group_id].push(socket.id);
+      // // Look for all of the messages for the group
+      // let msgs = await Message.find({ group_id });
+      // // Send to the incoming client
+      // msgs.forEach((msg) => {
+      //   socket.emit("message", msg);
+      // });
     });
     socket.on("unsubscribe", (data) => {
       let { group_id } = data;
-      if (!(group_id in this.subscrib_list)) return;
-      let new_sub_list = this.subscrib_list[group_id].filter(
-        (e) => e != socket.id
-      );
-      this.subscrib_list[group_id] = new_sub_list;
+      socket.leave(group_id);
+      // if (!(group_id in this.subscrib_list)) return;
+      // let new_sub_list = this.subscrib_list[group_id].filter(
+      //   (e) => e != socket.id
+      // );
+      // this.subscrib_list[group_id] = new_sub_list;
     });
   }
   verifyAccessToken(socket) {
